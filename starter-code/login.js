1b417707fab5057ef14293e9b7fef1a680ed860d
emmabennett <emma.j.bennett15@gmail.com>
2018-03-13 19:43:51 -0700

    was able to get get it to push through but its not checkcing password accuracy

--- starter-code/login.js
+++ starter-code/login.js
@@ -5,25 +5,25 @@ let userLogin = [
 			userName: "bellabug",
 			password: "bug"
 		}
-	];
+	]
 
 var userPassword;
-var fail;
+var fail = 0;
 var loggedIn = false;
 
 
 window.prompt("Enter password for user bellabug")
- while (!loggedIn && fail < 3) {
-	if (userPassword === userLogin[i].password) {
+while (!loggedIn && fail < 3) {
+	if (userPassword === userLogin.password) {
 		alert("Welcome back")
 		loggedIn = true;
 	} else {
 		alert("Password did not match, try again")
-		fail + 1;
+		fail += 1;
 	}
+
+}
 
 	if (fail === 3) {
 		alert("no more attempts can be made")
-	};
-
-}
+	}
