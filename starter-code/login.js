2a252ed78e47cfa00b32db4c5c673636c52cea87
emmabennett <emma.j.bennett15@gmail.com>
2018-03-13 19:25:00 -0700

    working on the login if loop

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,19 @@
 console.log("login.js loaded");
+
+let userLogin = [
+		{
+			userName: "bellabug",
+			password: "bug"
+		}
+	];
+
+var userPassword;
+
+
+window.prompt("Enter password for user bellabug")
+
+	if (userPassword === userLogin.password) {
+		alert("Welcome back")
+	} else {
+		alert("You are not welcome")
+	}
