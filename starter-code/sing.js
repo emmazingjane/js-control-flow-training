8ee23158f989e0c1986a1a214c77ae1ae14bf513
emmabennett <emma.j.bennett15@gmail.com>
2018-03-14 08:45:04 -0700

    got the console to log the song

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -2,13 +2,12 @@ console.log("sing.js loaded");
 
 
 
-var bottles = prompt("how many bottles of beer are there?");
-var numberBottles;
+// var bottles = prompt("how many bottles of beer are there?");
+var numberBottles = 5;
 
-while (numberBottles > 1) {
-	console.log( numberBottles + " " + bottles +
-		" "+ "on the wall")
-	console.log( numberBottles + " " + " of beer")
+while (numberBottles > 0){
+	console.log( numberBottles + " of beer on the wall")
+	console.log( numberBottles + " bottles of beer")
 	console.log("Take one down and pass it around,")
 	numberBottles = numberBottles - 1
 }
