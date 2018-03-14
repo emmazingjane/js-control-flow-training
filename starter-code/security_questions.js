5a4de737bd8072f25787f5301bf6ccb24a5bbfdf
emmabennett <emma.j.bennett15@gmail.com>
2018-03-13 19:13:11 -0700

    debugging secrurity prompts

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -3,24 +3,22 @@ console.log("security_questions.js loaded");
 var securityQuestions = [
 	{ question: "What is your date of birth?",
 	  expectedAnswer: "Canada"
-	};
+	},
 
 	{ question: "What is your first pet's name",
 	  expectedAnswer: "Fido"
-	};
+	},
 
 	{ question: "What is your favorite color?",
 	  expectedAnswer: "purple"
-	};
+	},
 ]
 
-for (var i < securityQuestions.length; i++) {
-	var userAnswer = securityQuestions.question[i], i + i
- 	if () {
- 	} else {
- 		window.prompt("Incorrect");
+for (var i = 0; i < securityQuestions.length; i++) {
+	var userAnswer = securityQuestions[i].question;
+ 	if (userAnswer !== securityQuestions[i].expectedAnswer) {
+ 		window.prompt("Incorrect unable to access acount")
  		break
- 	}
-
+ 	};
 
 }
