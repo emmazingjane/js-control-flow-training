b6630b903bbeb2ae112818673408c7406b1d50b7
emmabennett <emma.j.bennett15@gmail.com>
2018-03-13 19:05:19 -0700

    first commit for security question, working on if statement

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,26 @@
 console.log("security_questions.js loaded");
+
+var securityQuestions = [
+	{ question: "What is your date of birth?",
+	  expectedAnswer: "Canada"
+	};
+
+	{ question: "What is your first pet's name",
+	  expectedAnswer: "Fido"
+	};
+
+	{ question: "What is your favorite color?",
+	  expectedAnswer: "purple"
+	};
+]
+
+for (var i < securityQuestions.length; i++) {
+	var userAnswer = securityQuestions.question[i], i + i
+ 	if () {
+ 	} else {
+ 		window.prompt("Incorrect");
+ 		break
+ 	}
+
+
+}
