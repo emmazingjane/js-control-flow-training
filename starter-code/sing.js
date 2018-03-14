766d90a2ff27b2d6504e1cbd7df323ab6467be05
emmabennett <emma.j.bennett15@gmail.com>
2018-03-14 05:44:59 -0700

    debuggin and trying to make it go through the loop

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,14 @@
 console.log("sing.js loaded");
+
+
+
+var bottles = prompt("how many bottles of beer are there?");
+var numberBottles;
+
+while (numberBottles > 1) {
+	console.log( numberBottles + " " + bottles +
+		" "+ "on the wall")
+	console.log( numberBottles + " " + " of beer")
+	console.log("Take one down and pass it around,")
+	numberBottles = numberBottles - 1
+}
