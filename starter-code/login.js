e4ac20cb33f44b387f189a979f274441523283de
emmabennett <emma.j.bennett15@gmail.com>
2018-03-13 19:37:02 -0700

    debugging

--- starter-code/login.js
+++ starter-code/login.js
@@ -8,12 +8,22 @@ let userLogin = [
 	];
 
 var userPassword;
+var fail;
+var loggedIn = false;
 
 
 window.prompt("Enter password for user bellabug")
-
-	if (userPassword === userLogin.password) {
+ while (!loggedIn && fail < 3) {
+	if (userPassword === userLogin[i].password) {
 		alert("Welcome back")
+		loggedIn = true;
 	} else {
-		alert("You are not welcome")
+		alert("Password did not match, try again")
+		fail + 1;
 	}
+
+	if (fail === 3) {
+		alert("no more attempts can be made")
+	};
+
+}
